c92c0d402fcce3e54ca56725cfb96ba9f20c1ad2
Jariel97 <jariel.gojar@gmail.com>
2020-02-21 11:51:42 +0530

    Adding the basic login and authorisation to firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+export default {
+  apiKey: "AIzaSyA75v4KYVAaQS0U7IprjU1w9ry_UE_PZw4",
+  authDomain: "utdlostfound.firebaseapp.com",
+  databaseURL: "https://utdlostfound.firebaseio.com",
+  projectId: "utdlostfound",
+  storageBucket: "utdlostfound.appspot.com",
+  messagingSenderId: "995689114684",
+  appId: "1:995689114684:web:e424feb5d2310d32592739"
+
+};
+
+  
